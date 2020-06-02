56852d81036532d159bbe21993d09d3425a5b372
Sophie Popow <sophie.popow@gmail.com>
2020-06-02 18:24:00 +0200

    BSTree Bug Fixes

--- Lab9/WirtualnyDziekanat.c
+++ Lab9/WirtualnyDziekanat.c
@@ -1,6 +1,6 @@
 //
 //  WirtualnyDziekanat.c
-//  
+//
 //
 //  Created by Sophie Popow on 01/06/2020.
 //
@@ -19,21 +19,21 @@ typedef struct Node {
 } Node;
 
 Node* create_node(int id,char *name) {
-    Node* student = (node*)malloc(sizeof(Node));
-    
+    Node* student = (Node*)malloc(sizeof(Node));
+
     student->name = malloc(MAX_NAME_SIZE*sizeof(char));
     strcpy(student->name,name);
-    
+
     student->id = id;
     student->left = NULL;
     student->right = NULL;
     student->parent = NULL;
-    
+
     return student;
 }
 
 void delete_tree(Node* root) {
-    if (root = NULL)
+    if (root == NULL)
         return;
     delete_tree(root->left);
     delete_tree(root->right);
@@ -41,22 +41,22 @@ void delete_tree(Node* root) {
 }
 
 void insert(Node* root, Node* student) {
-    parent = NULL;
-    
+    Node* parent = NULL;
+
     while(root != NULL){
         parent = root;
-        
+
         if (root->id < student->id)
-            root = root.right;
+            root = root->right;
         else
-            root = root.left
+            root = root->left;
     }
-    
-    if (parent->id < student.id)
+
+    if (parent->id < student->id)
         parent->right = student;
     else
         parent->left = student;
-    
+
     student->parent = parent;
 }
 
@@ -65,37 +65,46 @@ Node* find(Node* root, int id) {
         return NULL;
     if(root->id == id)
         return root;
-    
+
     while(root != NULL && root->id != id){
         if(root->id < id)
-            root = root.right;
+            root = root->right;
         else if (root->id > id)
-            root = root.left;
+            root = root->left;
         else if (root->id == id)
             return root;
     }
     return NULL;
 }
 
-void delete(Node* root, int id) {
-    if( root == NULL)
+Node* minimum(Node* root) {
+    if (root == NULL)
         return NULL;
+    if (root->left == NULL)
+        return root;
+
+    return minimum(root->left);
+}
+
+Node* delete(Node* root, int id) {
+    if( root == NULL)
+        return root;
     if( id < root->id )
         root->left = delete(root->left, id);
     else if( id > root->id )
         root->right = delete(root->right, id);
     else {
         if( root->right == NULL){
-            Node* tmp = root->right;
-            free(root);
-            return tmp;
-        }
-        else if( root->left == NULL){
             Node* tmp = root->left;
             free(root);
             return tmp;
         }
-        
+        else if( root->left == NULL){
+            Node* tmp = root->right;
+            free(root);
+            return tmp;
+        }
+
         Node* tmp = minimum(root->right);
         root->id = tmp->id;
         root->name = tmp->name;
@@ -104,53 +113,45 @@ void delete(Node* root, int id) {
     return root;
 }
 
-Node* minimum(Node* root) {
-    if (root == NULL)
-        return NULL;
-    if (root->left == NULL)
-        return root;
-    
-    return minimum(root->left);
-}
 
 int main() {
     int n, r, f;
     scanf("%d", &n);
     scanf("%d", &r);
     scanf("%d", &f);
-    
-    node* root = NULL;
-    int number;
+
+    Node* root = NULL;
+    int id;
     char* name = malloc(MAX_NAME_SIZE*sizeof(char));
-    
+
     if (n-- > 0)
     {
-        scanf("%d", &number);
+        scanf("%d", &id);
         scanf("%s", name);
-        root = create_node(number, name);
+        root = create_node(id, name);
     }
-    
+
     while (n-- > 0)
     {
-        scanf("%d", &number);
+        scanf("%d", &id);
         scanf("%s", name);
-        
-        insert(root, create_node(number, name));
+
+        insert(root, create_node(id, name));
     }
-    
+
     while (r-- > 0)
     {
-        scanf("%d", &number);
-        root = delete(root, number);
+        scanf("%d", &id);
+        root = delete(root, id);
     }
-    
+
     while (f-- > 0)
     {
-        scanf("%d", &number);
-        node* x = find(root, number);
+        scanf("%d", &id);
+        Node* x = find(root, id);
         printf("%s\n", x == NULL ? "NOTFOUND" : x->name);
     }
-    
+
     delete_tree(root);
     free(name);
 }
